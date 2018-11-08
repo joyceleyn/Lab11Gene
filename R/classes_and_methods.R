06f7adc2171e29f2f19878d09ffa466c6e133668
Njuguna <njuguna2@illinois.edu>
2018-11-08 11:03:42 -0600

    initial commit

--- R/classes_and_methods.R
+++ R/classes_and_methods.R
@@ -20,5 +20,26 @@ Gene <- function(ID, symbol, ontology, CDS){
   
   out <- list(ID = ID, symbol = symbol, ontology = as.integer(ontology),
               CDS = CDS)
+  class(out) <- c("Gene", class(out))
   return(out)
 }
+
+#function to find protein length
+proteinLength <- function(object, ...){
+  UseMethod("proteinLength", object)
+}
+proteinLength.Gene <- function(object, ...){
+  amino_acid_len <- nchar(object$CDS)/3
+  return(amino_acid_len)
+}
+
+#print some information from the Gene object
+
+print.Gene <- function(object, ...){
+  cat(paste("Gene Id:", object$ID ),
+      paste("Symbol:",object$symbol),
+      paste("Length in bp:",nchar(object$CDS)),
+      paste("length in AA:",proteinLength(object)),
+      sep = "\n")
+}
+
